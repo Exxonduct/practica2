34d3e0fd7c53910187570b6fac4405383bcdb8f2
Exxonduct <zeos21k@gmail.com>
2023-01-24 18:33:12 +0100

    pasadas las opciones a un metodo que las printea

--- src/main/java/daw/programacion/obra/Main.java
+++ src/main/java/daw/programacion/obra/Main.java
@@ -36,6 +36,19 @@ public class Main {
         }
     }
 
+    public static void printOpciones(){
+        System.out.println("");
+        System.out.println("1. Visualizar las obras de arte existentes en la galería. Permite conocer todas las obras que se exponen.");
+        System.out.println("2. Dar de alta una nueva obra de arte.");
+        System.out.println("3. Modificar los datos de una de las obras de arte expuestas.");
+        System.out.println("4. Visualizar los datos de una de las obras de arte expuestas.");
+        System.out.println("5. Obtener el precio de venta de una de las obras de arte expuestas.");
+        System.out.println("6. Imprimir una etiqueta para clasificar una de las obras expuestas y dar información adicional a los visitantes de la galería.");
+        System.out.println("7. Salir");
+        System.out.println("");
+        System.out.print("Indique lo que quiere realizar: ");
+    }
+
 
     public static void main( String[] args )
     {
@@ -53,16 +66,7 @@ public class Main {
         Scanner read = new Scanner(System.in);
         while(x != 7){
 
-            System.out.println("");
-            System.out.println("1. Visualizar las obras de arte existentes en la galería. Permite conocer todas las obras que se exponen.");
-            System.out.println("2. Dar de alta una nueva obra de arte.");
-            System.out.println("3. Modificar los datos de una de las obras de arte expuestas.");
-            System.out.println("4. Visualizar los datos de una de las obras de arte expuestas.");
-            System.out.println("5. Obtener el precio de venta de una de las obras de arte expuestas.");
-            System.out.println("6. Imprimir una etiqueta para clasificar una de las obras expuestas y dar información adicional a los visitantes de la galería.");
-            System.out.println("7. Salir");
-            System.out.println("");
-            System.out.print("Indique lo que quiere realizar: ");
+            printOpciones();
             x = read.nextInt();
 
             switch(x){
