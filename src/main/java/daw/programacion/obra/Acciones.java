95a4ea6ec8003cb5beb3551c47503939359b342c
Exxonduct <zeos21k@gmail.com>
2023-01-26 17:27:09 +0100

    creado opcion 1 para se cargue desde obradearte

--- src/main/java/daw/programacion/obra/Acciones.java
+++ src/main/java/daw/programacion/obra/Acciones.java
@@ -2,5 +2,20 @@ package daw.programacion.obra;
 
 public class Acciones {
     
+    public static void printOpciones(){
+        System.out.println("");
+        System.out.println("1. Visualizar las obras de arte existentes en la galería. Permite conocer todas las obras que se exponen.");
+        System.out.println("2. Dar de alta una nueva obra de arte.");
+        System.out.println("3. Modificar los datos de una de las obras de arte expuestas.");
+        System.out.println("4. Visualizar los datos de una de las obras de arte expuestas.");
+        System.out.println("5. Obtener el precio de venta de una de las obras de arte expuestas.");
+        System.out.println("6. Imprimir una etiqueta para clasificar una de las obras expuestas y dar información adicional a los visitantes de la galería.");
+        System.out.println("7. Salir");
+        System.out.println("");
+        System.out.print("Indique lo que quiere realizar: ");
+        
+    }
+
+
 
 }
--- src/main/java/daw/programacion/obra/Main.java
+++ src/main/java/daw/programacion/obra/Main.java
@@ -1,220 +1,22 @@
 package daw.programacion.obra;
 
-import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void printPinturas(Pintura[] pinturas){
-        System.out.println("");
-        System.out.println("Pinturas:");
-        for(int i = 0;i<pinturas.length;i++){
-            System.out.print("ID: "+pinturas[i].getId());
-            System.out.print(" Nombre: "+pinturas[i].getNombre());
-            System.out.print(" Autor: "+pinturas[i].getAutor());
-            System.out.print(" Técnica: "+pinturas[i].getTecnica());
-            System.out.print(" Precio: "+pinturas[i].getPrecio()+" euros");
-            System.out.print(" Altura: "+pinturas[i].getAltura()+"m");
-            System.out.print(" Peso: "+pinturas[i].getPeso()+"t");
-            System.out.print(" Piezas: "+pinturas[i].getNumeroPiezas());
-            System.out.print(" Descripción: "+pinturas[i].getDescripcion());
-            System.out.println("");
-
-        }
-    }
-
-    public static void printEsculturas(Escultura[] esculturas){
-        System.out.println("");
-        System.out.println("Esculturas: ");
-        for(int i = 0;i<esculturas.length;i++){
-            System.out.print("ID: "+esculturas[i].getId());
-            System.out.print(" Nombre: "+esculturas[i].getNombre());
-            System.out.print(" Autor: "+esculturas[i].getAutor());
-            System.out.print(" Técnica: "+esculturas[i].getMaterial());
-            System.out.print(" Precio: "+esculturas[i].getPrecio()+" euros");
-            System.out.print(" Altura: "+esculturas[i].getAltura()+"m");
-            System.out.print(" Peso: "+esculturas[i].getPeso()+"t");
-            System.out.print(" Piezas: "+esculturas[i].getNumeroPiezas());
-            System.out.print(" Descripción: "+esculturas[i].getDescripcion());
-            System.out.println("");
-
-        }
-    }
-
-    public static void printObras(Escultura[] esculturas, Pintura[] pinturas){
-        printPinturas(pinturas);
-        printEsculturas(esculturas);
-    }
-
-    public static void printOpciones(){
-            System.out.println("");
-            System.out.println("1. Visualizar las obras de arte existentes en la galería. Permite conocer todas las obras que se exponen.");
-            System.out.println("2. Dar de alta una nueva obra de arte.");
-            System.out.println("3. Modificar los datos de una de las obras de arte expuestas.");
-            System.out.println("4. Visualizar los datos de una de las obras de arte expuestas.");
-            System.out.println("5. Obtener el precio de venta de una de las obras de arte expuestas.");
-            System.out.println("6. Imprimir una etiqueta para clasificar una de las obras expuestas y dar información adicional a los visitantes de la galería.");
-            System.out.println("7. Salir");
-            System.out.println("");
-            System.out.print("Indique lo que quiere realizar: ");
-    }
-
-
     public static void main( String[] args )
     {
 
-        Pintura guernica = new Pintura(001, "Guernica", "P.Picasso", 1000, 5, 2, 5, "cuadro guerra civil", "Óleo");
-        Pintura laVie = new Pintura(002, "La vie", "P.Picasso", 200, 1, 1, 1, "óleo", "Óleo");
-        Pintura elSueño = new Pintura(003, "El Sueño", "P.Picasso", 300, 1.3, 1, 5, "óleo", "Óleo");
-        Pintura retratoDeDoraMaar = new Pintura(004, "Retrato de Dora Maar", "P.Picasso", 400, 1, 0.8, 1, "óleo", "Óleo");
-        Escultura elPielRoja = new Escultura(005, "El piel roja", "U.Checa", 350, 1, 0.8, 1, "escultura", "null");
-        
-        Pintura[] pinturas = {guernica, laVie, elSueño, retratoDeDoraMaar};
-        Escultura[] esculturas = {elPielRoja};
 
         int x = 0;
         Scanner read = new Scanner(System.in);
         while(x != 7){
 
-            printOpciones();
+            Acciones.printOpciones();
             x = read.nextInt();
-
             switch(x){
                 case 1:
-                    printObras(esculturas, pinturas);
-                continue;
-                case 2:
-                System.out.print("Introduzca el ID de la nueva obra: ");
-                int id = read.nextInt();
-                System.out.print("Introduzca el tipo de obra que quiere crear(Pintura/Escultura): ");
-                String s = read.nextLine();
-                String tipo = read.nextLine();
-                System.out.print("Introduzca el Nombre de la obra: ");
-                String nombre = read.nextLine();
-                System.out.print("Introduzca el nombre del Autor: ");
-                String autor = read.nextLine();
-                System.out.print("Introduzca el precio: ");
-                double precio = read.nextDouble();
-                System.out.print("Introduzca la altura: ");
-                double altura = read.nextDouble();
-                System.out.print("Introduzca el peso: ");
-                double peso = read.nextDouble();
-                System.out.print("Introduzca el número de piezas: ");
-                int piezas = read.nextInt();
-                String g = read.nextLine();
-                System.out.print("Introduzca la descripción: ");
-                String desc = read.nextLine();
-                if(tipo.equals("Pintura")){
-                    System.out.print("Introduzca la Técnica de la Pintura: ");
-                    String tecnica = read.nextLine();
-                    Pintura creada1 = new Pintura(id, nombre, autor, precio, altura, peso, piezas, desc, tecnica);
-                    Pintura[] pinturasNueva = new Pintura[pinturas.length+1];
-
-                    for(int i=0;i<pinturas.length;i++){
-                        pinturasNueva[i] = pinturas[i];
-                    }
-
-                    pinturas = new Pintura[pinturasNueva.length];
-
-                    for(int i=0;i<pinturas.length;i++){
-                        pinturas[i] = pinturasNueva[i];
-                    }
-
-                    pinturas[pinturas.length-1] = creada1;
-
-                }
-                else if(tipo.equals("Escultura")){
-                    System.out.print("Introduzca el Material de la Escultura: ");
-                    String material = read.nextLine();
-                    Escultura creada1 = new Escultura(id, nombre, autor, precio, altura, peso, piezas, desc, material);
-                    Escultura[] esculturasNueva = new Escultura[esculturas.length+1];
-
-                    for(int i=0;i<esculturas.length;i++){
-                        esculturasNueva[i] = esculturas[i];
-                    }
-
-                    esculturas = new Escultura[esculturasNueva.length];
-
-                    for(int i=0;i<esculturas.length;i++){
-                        esculturas[i] = esculturasNueva[i];
-                    }
-
-                    esculturas[esculturas.length-1] = creada1;
-
-                }
-
-            
-
-                    
-                continue;
-                case 3:
-                    printObras(esculturas, pinturas);
-                    System.out.println(" ");
-                    System.out.print("Indique qué obra quiere modificar escribiendo el ID: ");
-                    int seleccion = read.nextInt();
-                    read.nextLine();
-                    for(int i = 0; i < esculturas.length;i++){
-                        if(esculturas[i].getId() == seleccion){
-                            System.out.print("Indica qué dato quieres cambiar: ");
-                            String datoACambiar = read.nextLine();
-                            switch(datoACambiar){
-                                case "id":
-                                    System.out.print("Introduce el nuevo id: ");
-                                    int nuevoId = read.nextInt();
-                                    esculturas[i].setId(nuevoId);
-                                    break;
-                                case "nombre":
-                                    System.out.print("Introduzca el nuevo nombre: ");
-                                    String nuevoNombre = read.nextLine();
-                                    esculturas[i].setNombre(nuevoNombre);
-                                    break;
-                                case "autor":
-                                    System.out.print("Introduzca el nuevo autor: ");
-                                    String nuevoAutor = read.nextLine();
-                                    esculturas[i].setAutor(nuevoAutor);
-                                    break;
-                                case "material":
-                                    System.out.print("Introduzca el nuevo material: ");
-                                    String nuevoMaterial = read.nextLine();
-                                    esculturas[i].setMaterial(nuevoMaterial);
-                                    break;
-                                case "precio":
-                                    System.out.print("Introduzca el nuevo precio: ");
-                                    double nuevoPrecio = read.nextDouble();
-                                    esculturas[i].setPrecio(nuevoPrecio);
-                                    break;
-                                case "altura":
-                                    System.out.print("Introduzca la nueva altura: ");
-                                    double nuevaAltura = read.nextDouble();
-                                    esculturas[i].setAltura(nuevaAltura);
-                                    break;
-                                case "peso":
-                                    System.out.print("Introduzca el nuevo peso: ");
-                                    double nuevoPeso = read.nextDouble();
-                                    esculturas[i].setPeso(nuevoPeso);
-                                    break;
-                                case "piezas":
-                                    System.out.print("Introduzca el nuevo número de piezas: ");
-                                    int nuevaPiezas = read.nextInt();
-                                    esculturas[i].setNumeroPiezas(nuevaPiezas);
-                                    break;
-                                case "descripcion":
-                                    System.out.print("Introduzca la nueva descripción: ");
-                                    String nuevaDescripcion = read.nextLine();
-                                    esculturas[i].setDescripcion(nuevaDescripcion);
-                                    break;
-                                
-                            }
-                        }
-                    }
-                    
-                    for(int i = 0;i<pinturas.length;i++){
-                        if(pinturas[i].getId() == seleccion){
-                            System.out.print("Indica qué dato quieres cambiar: ");
-                            String datoACambiar = read.nextLine();
-                        }
-                    }
-
+                    ObraDeArte.printObras();
                 continue;
                 case 4: 
 
